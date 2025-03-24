e839cea7476023310d8eb7837f893cf5649eab2c
nissy0903 <nisihiro44@gmail.com>
2025-03-24 15:01:24 +0900

    add github url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,10 +9,12 @@ export default function Home() {
             <h1>制作物1 : Toolbox</h1>
           </a>
           <p>便利機能を集めたアプリを制作しました。</p>
+          <a href="https://github.com/nissy0903/portfolio-site.git">github</a>
           <a href="https://dynamic-pie-0fcaad.netlify.app/" target="_blank" rel="noopener noreferrer">
             <h1>制作物2 : 林檎ゲーム</h1>
           </a>
           <p> 自分の地元の青森県から着想を得て、林檎を時間内に収穫する数を競うゲームを作成しました。制限時間を設けてゲームに緊迫感が出るように工夫しました。また、プレイヤーを楽しませるため、点数に応じてスコア画面のメッセージが変化する、林檎の色がランダムに変化する、といったギミックを加えました。</p>
+          <a href="https://github.com/nissy0903/apple-game.git">github</a>
         </div>
       </main>
       <footer className={styles.footer}>
